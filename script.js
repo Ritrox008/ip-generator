b14a2cbfcde5fb10f837e6d4c62b3b9535891af2
Ritrox008 <remi.leflohic@outlook.fr>
2024-05-15 16:55:14 +0200

    opti génération

--- script.js
+++ script.js
@@ -14,8 +14,11 @@ for (let index = 0; index < listeInputRadio.length; index++) {
         } else {
             if (event.target.value === "2") {
                 classe = 2  //Classe B
-            } else { 
+            } else {
+                if (event.target.value === "3") {
                 classe = 3  //Classe C
+            } else {
+                console.log("Bug class choice (line 11)")
             }
         }
         //console.log(classe)
@@ -24,43 +27,33 @@ for (let index = 0; index < listeInputRadio.length; index++) {
 
 let reponse
 let monBouton = document.getElementById("monBouton");
+//Définition variables min et max de génération//
+let max                                        //
+let min                                        //
+//Définition variables min et max de génération//
 monBouton.addEventListener("click", function () {
     if (classe === 1) {
-        let nbr1 = Math.floor(Math.random() * (126+1)) + 0;
-        nbr1 = String(nbr1)
-        let nbr2 = Math.floor(Math.random() * 255) + 0;
-        nbr2 = String(nbr2)
-        let nbr3 = Math.floor(Math.random() * 255) + 0;
-        nbr3 = String(nbr3)
-        let nbr4 = Math.floor(Math.random() * 255) + 0;
-        nbr4 = String(nbr4)
-        reponse = nbr1 + "." + nbr2 + "." + nbr3 + "." + nbr4
-        console.log(reponse)
+        min = 0
+        max = 126
     }
     if (classe === 2) {
-        let nbr1 = Math.floor(Math.random() * (191-128+1)) + 128;
-        nbr1 = String(nbr1)
-        let nbr2 = Math.floor(Math.random() * 255) + 0;
-        nbr2 = String(nbr2)
-        let nbr3 = Math.floor(Math.random() * 255) + 0;
-        nbr3 = String(nbr3)
-        let nbr4 = Math.floor(Math.random() * 255) + 0;
-        nbr4 = String(nbr4)
-        reponse = nbr1 + "." + nbr2 + "." + nbr3 + "." + nbr4
-        console.log(reponse)
+        min = 128
+        max = 191
     }
     if (classe === 3) {
-        let nbr1 = Math.floor(Math.random() * (223-192+1)) + 192;
-        nbr1 = String(nbr1)
-        let nbr2 = Math.floor(Math.random() * 255) + 0;
-        nbr2 = String(nbr2)
-        let nbr3 = Math.floor(Math.random() * 255) + 0;
-        nbr3 = String(nbr3)
-        let nbr4 = Math.floor(Math.random() * 255) + 0;
-        nbr4 = String(nbr4)
-        reponse = nbr1 + "." + nbr2 + "." + nbr3 + "." + nbr4
-        console.log(reponse)
+        min = 192
+        max = 223
     }
+    let nbr1 = Math.floor(Math.random() * (max-min+1)) + min;
+    nbr1 = String(nbr1)
+    let nbr2 = Math.floor(Math.random() * 255) + 0;
+    nbr2 = String(nbr2)
+    let nbr3 = Math.floor(Math.random() * 255) + 0;
+    nbr3 = String(nbr3)
+    let nbr4 = Math.floor(Math.random() * 255) + 0;
+    nbr4 = String(nbr4)
+    reponse = nbr1 + "." + nbr2 + "." + nbr3 + "." + nbr4
+    console.log(reponse)
 });
 
 
